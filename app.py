00fa80458226f816b45f6559099a01cf025a4244
Saravanan Kandaswamy <saravanan.kandaswamy@3ds.com>
2020-06-14 10:53:54 -0400

    few more changes

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
+    return render_template('index.html')
+
+@app.route('/details')
+def details():
     return render_template('index.html')
 
 @app.route('/parse_data',methods=['GET','POST'])
